b91523cc4145afcfea26eb7dd59c61c2ddd8ecc2
threepat1 <jtojung@gmail.com>
2020-12-16 03:29:59 +0700

    first

---
+++ Assets/Script/AfterEndTrack.cs
@@ -0,0 +1,21 @@
+﻿using RenderHeads.Media.AVProVideo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AfterEndTrack : MonoBehaviour
+{
+    private IMediaControl control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
---
+++ Assets/Script/PanelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PanelManager : MonoBehaviour
+{
+    public GameObject engPopup, thaiPopup, mainMenu;
+
+    public void PopUp1()
+    {
+        engPopup.SetActive(true);
+        thaiPopup.SetActive(false);
+        mainMenu.SetActive(false);
+    }
+    public void PopUp2()
+    {
+        engPopup.SetActive(false);
+        thaiPopup.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    public void Cancel()
+    {
+        engPopup.SetActive(false);
+        thaiPopup.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void GoToVideo1()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void GoToVideo2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
